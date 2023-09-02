af793fc01c7f49bda63e88cb6ecd7a2d42f885c5
unknown <mshayan02@gmail.com>
2023-09-02 14:08:10 +0530

    Alpha_V02

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,82 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from 'three';
+import { ARButton } from 'three/examples/jsm/webxr/ARButton';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+var container;
+var camera, scene, renderer;
+var controller;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+init();
+animate();
+
+function init() {
+
+  container = document.createElement( 'div' );
+  document.body.appendChild( container );
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
+
+  var light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+  light.position.set( 0.5, 1, 0.25 );
+  scene.add( light );
+
+  //
+
+  renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.xr.enabled = true;
+  container.appendChild( renderer.domElement );
+
+  //
+
+  document.body.appendChild( ARButton.createButton( renderer ) );
+
+  //
+
+  var geometry = new THREE.CylinderBufferGeometry( 0, 0.05, 0.2, 32 ).rotateX( Math.PI / 2 );
+
+  function onSelect() {
+
+    var material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
+    mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
+    scene.add( mesh );
+
+  }
+
+  controller = renderer.xr.getController( 0 );
+  controller.addEventListener( 'select', onSelect );
+  scene.add( controller );
+
+  //
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+//
+
+function animate() {
+
+  renderer.setAnimationLoop( render );
+
+}
+
+function render() {
+
+  renderer.render( scene, camera );
+
+}
